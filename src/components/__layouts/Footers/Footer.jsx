b9a76bb331a2543df85e7bd846d76e8fcd5aec27
Kongkon Jowarder <kongkon4545@gmail.com>
2024-09-27 19:22:39 +0600

    footer large device design

--- src/components/__layouts/Footers/Footer.jsx
+++ src/components/__layouts/Footers/Footer.jsx
@@ -2,7 +2,9 @@
 import Link from "next/link";
 
 import Image from "next/image";
-
+import { FaFacebookF } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 import "./footer.css";
 
 const Footer = () => {
@@ -10,307 +12,113 @@ const Footer = () => {
     <footer className="">
       {/* large device  */}
       <div className="hidden xl:block">
-        <div className="footer__bg container py-[100px]">
-          <div className="flex itmes-center gap-[50px] 5xl:gap-[92px]">
-            <span>
-              <svg
-                className="mt-6"
-                xmlns="http://www.w3.org/2000/svg"
-                width="auto"
-                height="30"
-                viewBox="0 0 682 30"
-                fill="none"
-              >
-                <path
-                  d="M681.65 15L667.217 0.566243L652.783 15L667.217 29.4338L681.65 15ZM0 17.5L667.217 17.5V12.5L0 12.5L0 17.5Z"
-                  fill="#C7A43C"
-                />
-              </svg>
-            </span>
-            <Image
-              src="/assets/images/footer_logo.png"
-              alt="footer logo"
-              width={401}
-              height={113}
-              className="w-[401px] h-[113px] mx-auto pb-[22px]"
-            />
-            <span>
-              <svg
-                className="mt-6"
-                xmlns="http://www.w3.org/2000/svg"
-                width="auto"
-                height="30"
-                viewBox="0 0 682 30"
-                fill="none"
-              >
-                <path
-                  d="M0.349609 15L14.7834 29.4338L29.2172 15L14.7834 0.566243L0.349609 15ZM682 12.5L14.7834 12.5V17.5L682 17.5V12.5Z"
-                  fill="#C7A43C"
-                />
-              </svg>
-            </span>
-          </div>
-          <p className="text-lg font-normal text-white text-center pb-[55px]">
-            Find the best nail salon in Mississauga for impeccable manicures &
-            pedicures. Expert service & stunning results await! Book now.
-          </p>
-          <div className="grid grid-cols-10 gap-10 5xl:gap-[54px]">
+        <div className="footer__bg container py-[60px]">
+          <div className="grid grid-cols-8 gap-10 5xl:gap-[54px]">
             {/* first div  */}
             <div className="col-span-2">
-              <p className="text-[25px] font-semibold text-white pb-[28px]">
-                Navigation
+              <Image
+                src="/assets/glitz/icons/footer_brand_logo.png"
+                alt="footer logo"
+                width={144}
+                height={148}
+                className="w-[144px] h-[148px] mx-auto pb-[22px]"
+              />
+              <p className="text-black text-lg font-bold pb-[5px]">GLITZ FLOORS & MORE</p>
+              <p className="text-lg font-bold text-white text-left leading-[30px]">
+              If you're in the Cypress area and looking for expert flooring and remodeling services, contact us today.
               </p>
-              <ul className="text-white">
-                <li className="text-lg font-medium">Blogs</li>
-                <li className="text-lg font-medium py-[26px]">Contact Us</li>
-                <li className="text-lg font-medium">Testimonial</li>
-                <li className="text-lg font-medium py-[26px]">Gallery</li>
-                <li className="text-lg font-medium">Sitemap</li>
-              </ul>
             </div>
             {/* second div  */}
             <div className="col-span-2">
-              <p className="text-[25px] font-semibold text-white pb-[28px]">
-                Ours Services
+              <p className="text-lg font-bold text-white pb-[17px] leading-[29px]">
+                Hours of Operations:
               </p>
               <ul className="text-white">
-                <li className="text-lg font-medium">Manicure</li>
-                <li className="text-lg font-medium py-[26px]">Pedicure</li>
-                <li className="text-lg font-medium">Waxing</li>
-                <li className="text-lg font-medium py-[26px]">Design</li>
-                <li className="text-lg font-medium">Artificial Nails</li>
-                <li className="text-lg font-medium pt-[26px]">
-                  Additional Services
+                <li className="text-base md:text-lg normal text-white leading-[27px]">Monday to Thursday</li>
+                <li className="text-base md:text-lg font-normal text-white leading-[27px] py-[14px]">
+                  10:00 am - 06:00 p.m.
+                </li>
+                <li className="text-base md:text-lg font-normal text-white leading-[27px]">Friday </li>
+                <li className="text-base md:text-lg font-normal text-white leading-[27px] py-[14px]">
+                  10:00 am - 05:00 p.m.
+                </li>
+                <li className="text-base md:text-lg font-normal text-white leading-[27px]">Saturday</li>
+                <li className="text-base md:text-lg font-normal text-white leading-[27px] py-[14px]">
+                  10:00 am - 02:00 p.m.
                 </li>
               </ul>
+              <p className="text-sm font-normal text-white leading-[21px]">
+                * We remain open during statutoryholidays and operate as per our
+                regular hours.
+              </p>
             </div>
             {/* third div  */}
-            <div className="col-span-3">
-              <div className="border-[4px] rounded-[8px] border-primary ">
-                <p className="text-[25px] font-semibold text-white text-center pb-[28px] pt-[8px]">
-                  Open Hours
+            <div className="col-span-2">
+              <div className="">
+                <p className="text-lg font-bold text-white pb-[17px] leading-[29px]">
+                  Our Services
                 </p>
-                <span>
-                  <svg
-                    className="px-[20px] 5xl:px-[50px]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="auto"
-                    height="24"
-                    viewBox="0 0 424 24"
-                    fill="none"
-                  >
-                    <path
-                      d="M0.452994 12L12 23.547L23.547 12L12 0.452994L0.452994 12ZM423.547 12L412 0.452994L400.453 12L412 23.547L423.547 12ZM12 14H412V10H12V14Z"
-                      fill="#C7A43C"
-                    />
-                  </svg>
-                </span>
-                <p className="text-lg font-medium text-white text-center pb-[23px]">
-                  Mon. ~ Fri. <br /> 10:00am ~ 7:00pm <br /> Sat. <br /> 9:00am
-                  ~ 6:00pm <br /> Sun. <br /> 11:00am ~ 5:00pm
-                </p>
+                <ul>
+                  <li className="text-base md:text-lg font-normal text-white leading-[27px]">&gt; Flooring Installations</li>
+                  <li className="text-base md:text-lg font-normal text-white leading-[27px] py-[14px]">&gt; Kitchen Remodeling</li>
+                  <li className="text-base md:text-lg font-normal text-white leading-[27px]">&gt; Bathroom Remodeling</li>
+                  <li className="text-base md:text-lg font-normal text-white leading-[27px] py-[14px]">&gt; Interior Painting</li>
+                  <li className="text-base md:text-lg font-normal text-white leading-[27px]">&gt; Interior Remodel</li>
+                </ul>
               </div>
             </div>
-            
-            {/* fourth div  */}
-            <div className="col-span-3">
-              <p className="text-[25px] font-semibold text-white pb-[28px]">
-                Contact Us
-              </p>
-              <ul>
-                <li className="flex items-center gap-[24px] text-lg font-medium">
-                  <Image src="/assets/icons/map.png" alt="map icon" width={21} height={21}/>
-                  <Link
-                    className="text-white"
-                    href="location : 1100 Burnhamthorpe Rd W Unit 16, Mississauga, ON L5C 4G4, Canada"
-                  >
-                    1100 Burnhamthorpe Rd W Unit 16, <br className="hidden 5xl:block"/> Mississauga, ON L5C 4G4,
-                    Canada
-                  </Link>
-                </li>
-                <li className="flex items-center gap-[24px] text-lg font-medium py-[26px]">
-                  <Image src="/assets/icons/phone.png" alt="phone icon" width={21} height={21}/>
-                  <Link className="text-white" href="tel : (905) 973-6669">
-                    (905) 973-6669
-                  </Link>
-                </li>
-                <li className="flex items-center gap-[24px] text-lg font-medium">
-                  <Image src="/assets/icons/email.png" alt="email icon" width={21} height={21}/>
-                  <Link
-                    className="text-white"
-                    href="mail : manager@lavishluxnailsandspa.com"
-                  >
-                    manager@lavishluxnailsandspa.com
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        {/* footer bottom  */}
-        <div className="bg-primary py-[17px] container flex justify-between items-center">
-          <div className="flex items-center gap-[15px]">
-            <p className="text-base font-semibold text-white">
-              WEBSITE DESIGN AND SEO BY :-
-            </p>
-            <Image
-              src="/assets/images/goKundu__fb.png"
-              alt="gokundu logo"
-              width={141}
-              height={36}
-            />
-          </div>
-          <div>
-            <p className="text-base font-medium text-white">
-              © All Copyright 2024 by Lavish Lux Nail Spa
-            </p>
-          </div>
-          <div className="flex items-center justify-between">
-            <p className="text-base font-medium text-white">
-              Terms & Condition
-            </p>
-            <p className="text-base font-medium text-white">Privacy Policy</p>
-          </div>
-        </div>
-      </div>
 
-      {/* medium device  */}
-      <div className="hidden lg:block xl:hidden">
-        <div className="footer__bg container py-[100px]">
-          <div className="flex itmes-center gap-[50px] 5xl:gap-[92px]">
-            <span>
-              <svg
-                className="mt-6"
-                xmlns="http://www.w3.org/2000/svg"
-                width="auto"
-                height="30"
-                viewBox="0 0 682 30"
-                fill="none"
-              >
-                <path
-                  d="M681.65 15L667.217 0.566243L652.783 15L667.217 29.4338L681.65 15ZM0 17.5L667.217 17.5V12.5L0 12.5L0 17.5Z"
-                  fill="#C7A43C"
-                />
-              </svg>
-            </span>
-            <Image
-              src="/assets/images/footer_logo.png"
-              alt="footer logo"
-              width={401}
-              height={113}
-              className="w-[401px] h-[113px] mx-auto pb-[22px]"
-            />
-            <span>
-              <svg
-                className="mt-6"
-                xmlns="http://www.w3.org/2000/svg"
-                width="auto"
-                height="30"
-                viewBox="0 0 682 30"
-                fill="none"
-              >
-                <path
-                  d="M0.349609 15L14.7834 29.4338L29.2172 15L14.7834 0.566243L0.349609 15ZM682 12.5L14.7834 12.5V17.5L682 17.5V12.5Z"
-                  fill="#C7A43C"
-                />
-              </svg>
-            </span>
-          </div>
-          <p className="text-lg font-normal text-white text-center pb-[55px]">
-            Find the best nail salon in Mississauga for impeccable manicures &
-            pedicures. Expert service & stunning results await! Book now.
-          </p>
-          <div className="grid grid-cols-7 gap-10 5xl:gap-[54px]">
-            {/* first div  */}
+            {/* fourth div  */}
             <div className="col-span-2">
-              <p className="text-[25px] font-semibold text-white pb-[28px]">
-                Navigation
-              </p>
-              <ul className="text-white">
-                <li className="text-lg font-medium">Blogs</li>
-                <li className="text-lg font-medium py-[26px]">Contact Us</li>
-                <li className="text-lg font-medium">Testimonial</li>
-                <li className="text-lg font-medium py-[26px]">Gallery</li>
-                <li className="text-lg font-medium">Sitemap</li>
-              </ul>
-            </div>
-            {/* second div  */}
-            <div className="col-span-2">
-              <p className="text-[25px] font-semibold text-white pb-[28px]">
-                Ours Services
-              </p>
-              <ul className="text-white">
-                <li className="text-lg font-medium">Manicure</li>
-                <li className="text-lg font-medium py-[26px]">Pedicure</li>
-                <li className="text-lg font-medium">Waxing</li>
-                <li className="text-lg font-medium py-[26px]">Design</li>
-                <li className="text-lg font-medium">Artificial Nails</li>
-                <li className="text-lg font-medium pt-[26px]">
-                  Additional Services
-                </li>
-              </ul>
-            </div>
-            {/* third div  */}
-            <div className="col-span-3">
-              <p className="text-[25px] font-semibold text-white pb-[28px]">
-                Contact Us
-              </p>
               <ul>
-                <li className="flex items-center gap-[24px] text-lg font-medium">
-                  <Image src="/assets/icons/map.png" alt="map icon" width={21} height={21}/>
+                <li className="flex items-center gap-[24px] text-base 5xl:text-lg font-normal leading-[29px]">
+                  <Image
+                    src="/assets/icons/map.png"
+                    alt="map icon"
+                    width={21}
+                    height={21}
+                  />
                   <Link
                     className="text-white"
-                    href="location : 1100 Burnhamthorpe Rd W Unit 16, Mississauga, ON L5C 4G4, Canada"
+                    href="location : 16726 Huffmeister Rd D400, Cypress, TX 77429, United States"
                   >
-                    1100 Burnhamthorpe Rd W Unit 16, Mississauga, ON L5C 4G4,
-                    Canada
+                    16726 Huffmeister Rd <br className="hidden 5xl:block"/> D400, Cypress, TX 77429, <br className="hidden 5xl:block" />{" "}
+                    United States
                   </Link>
                 </li>
-                <li className="flex items-center gap-[24px] text-lg font-medium py-[26px]">
-                  <Image src="/assets/icons/phone.png" alt="phone icon" width={21} height={21}/>
-                  <Link className="text-white" href="tel : (905) 973-6669">
-                    (905) 973-6669
+                <li className="flex items-center gap-[24px] text-base 5xl:text-lg font-normal leading-[29px] py-[27px]">
+                  <Image
+                    src="/assets/icons/phone.png"
+                    alt="phone icon"
+                    width={21}
+                    height={21}
+                  />
+                  <Link className="text-white" href="tel : +1 281-758-5450">
+                    +1 281-758-5450
                   </Link>
                 </li>
-                <li className="flex items-center gap-[24px] text-lg font-medium">
-                  <Image src="/assets/icons/email.png" alt="email icon" width={21} height={21}/>
+                <li className="flex items-center gap-[24px] text-base 5xl:text-lg font-normal leading-[29px]">
+                  <Image
+                    src="/assets/icons/email.png"
+                    alt="email icon"
+                    width={21}
+                    height={21}
+                  />
                   <Link
                     className="text-white"
-                    href="mail : manager@lavishluxnailsandspa.com"
+                    href="mail : info@glitzfloors&more.us"
                   >
-                    manager@lavishluxnailsandspa.com
+                    info@glitzfloors&more.us
                   </Link>
                 </li>
               </ul>
-            </div>
-          </div>
-          {/* fourth div  */}
-          <div className="w-1/2 mx-auto pt-[35px]">
-              <div className="border-[4px] rounded-[8px] border-primary ">
-                <p className="text-[25px] font-semibold text-white text-center pb-[28px] pt-[8px]">
-                  Open Hours
-                </p>
-                <span>
-                  <svg
-                    className="px-[20px] 5xl:px-[50px]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="auto"
-                    height="24"
-                    viewBox="0 0 424 24"
-                    fill="none"
-                  >
-                    <path
-                      d="M0.452994 12L12 23.547L23.547 12L12 0.452994L0.452994 12ZM423.547 12L412 0.452994L400.453 12L412 23.547L423.547 12ZM12 14H412V10H12V14Z"
-                      fill="#C7A43C"
-                    />
-                  </svg>
-                </span>
-                <p className="text-lg font-medium text-white text-center pb-[23px]">
-                  Mon. ~ Fri. <br /> 10:00am ~ 7:00pm <br /> Sat. <br /> 9:00am
-                  ~ 6:00pm <br /> Sun. <br /> 11:00am ~ 5:00pm
-                </p>
+              <div className="flex items-center gap-[30px] pt-[29px]">
+                <FaInstagram className="w-[21px] h-[21px]"/>
+                <FaTiktok className="w-[21px] h-[21px]"/>
+                <FaFacebookF className="w-[21px] h-[21px]"/>
               </div>
             </div>
+          </div>
         </div>
         {/* footer bottom  */}
         <div className="bg-primary py-[17px] container flex justify-between items-center">
@@ -327,10 +135,10 @@ const Footer = () => {
           </div>
           <div>
             <p className="text-base font-medium text-white">
-              © All Copyright 2024 by Lavish Lux Nail Spa
+              © All Copyright 2024 by GLITZ FLOORS & MORE
             </p>
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between gap-[5px]">
             <p className="text-base font-medium text-white">
               Terms & Condition
             </p>
