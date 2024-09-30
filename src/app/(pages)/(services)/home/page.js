50faa9c93e9bae8470fbf431b0b01854abf1b1b3
Kongkon Jowarder <kongkon4545@gmail.com>
2024-09-30 13:23:51 +0600

    home banner section add

--- src/app/(pages)/(services)/home/page.js
+++ src/app/(pages)/(services)/home/page.js
@@ -1,9 +1,10 @@
+import Home from '@/components/services/home/Home';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            
+            <Home/>
         </div>
     );
 };
