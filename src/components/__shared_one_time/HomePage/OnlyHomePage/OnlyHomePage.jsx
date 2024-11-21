917c0c9f7d8d65ed3efb63b41e20d2a7557c82ab
Kongkon Jowarder <kongkon4545@gmail.com>
2024-11-21 19:00:58 +0600

    banner section add

---
+++ src/components/__shared_one_time/HomePage/OnlyHomePage/OnlyHomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner/Banner';
+
+const OnlyHomePage = () => {
+    return (
+        <div>
+            <section>
+                <Banner/>
+            </section>
+        </div>
+    );
+};
+
+export default OnlyHomePage;
