07300dd22364afa452ad2bf2d16fadae6c06852e
Kongkon Jowarder <kongkon4545@gmail.com>
2024-11-28 00:20:46 +0600

    small device problem solve

--- src/components/__layouts/Navbar/Header/NavHeader.jsx
+++ src/components/__layouts/Navbar/Header/NavHeader.jsx
@@ -26,6 +26,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { LuPhoneCall } from "react-icons/lu";
+import { IoMenuSharp } from "react-icons/io5";
 
 // our services nav menu
 const our_service_menu_items = {
@@ -44,7 +45,6 @@ const our_service_menu_items = {
     },
   ],
 };
-
 
 function OurServiceManu() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -78,7 +78,7 @@ function OurServiceManu() {
             role="button"
           >
             <ListItem
-              className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal -mt-[9px]"
+              className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal -mt-[9px]"
               selected={isMenuOpen || isMobileMenuOpen}
               onClick={() => setIsMobileMenuOpen((cur) => !cur)}
             >
@@ -144,21 +144,24 @@ function OurServiceManu() {
 // our services nav list end
 function NavList() {
   return (
-    <List className="my-[11px] p-0 lg:mt-0 lg:mb-0 lg:flex-row md:items-center justify-between px-[20px] 5xl:px-[24px] border-0 outline-none">
+    <List className="my-[11px] p-0 lg:mt-0 lg:mb-0 lg:flex-row md:items-center justify-between px-0 md:px-[20px] 5xl:px-[24px] border-0 outline-none ">
+      <div className="border-b border-black">
       <Typography
         as={Link}
         href="/"
         variant="small"
         color="blue-gray"
-        className="font-medium rounded-full text-sm lg:text-sm xl:text-base 2xl:text-lg"
+        className=" font-medium rounded-full text-sm lg:text-sm xl:text-base 2xl:text-lg"
       >
         <div>
-          <ListItem className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
+          <ListItem className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
             Home
           </ListItem>
         </div>
       </Typography>
+      </div>
 
+      <div className="border-b border-black">
       <Typography
         as={Link}
         href="/about-us"
@@ -166,12 +169,13 @@ function NavList() {
         color="blue-gray"
         className="font-medium rounded-full text-sm md:text-sm xl:text-base 2xl:text-lg"
       >
-        <div>
-          <ListItem className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
+        <ListItem className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
             About
           </ListItem>
-        </div>
       </Typography>
+          </div>
+      
+      <div className="border-b border-black">
       <Typography
         as={Link}
         href="/gallery"
@@ -180,12 +184,14 @@ function NavList() {
         className="font-medium rounded-full text-sm md:text-sm xl:text-base 2xl:text-lg"
       >
         <div>
-          <ListItem className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
+          <ListItem className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
             Gallery
           </ListItem>
         </div>
       </Typography>
-
+        </div>
+      
+      <div className="border-b border-black">
       <Typography
         as={Link}
         href="/kitchen-remodel-in-cypress"
@@ -194,11 +200,15 @@ function NavList() {
         className="font-medium rounded-full text-sm md:text-sm xl:text-base 2xl:text-lg"
       >
         <div>
-          <ListItem className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
+          <ListItem className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
             Kitchen Remodeling
           </ListItem>
         </div>
       </Typography>
+        </div>
+
+      
+      <div className="border-b border-black">
       <Typography
         as={Link}
         href="/bathroom-remodel-in-cypress-tx"
@@ -207,16 +217,19 @@ function NavList() {
         className="font-medium rounded-full text-sm md:text-sm xl:text-base 2xl:text-lg"
       >
         <div>
-          <ListItem className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
+          <ListItem className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center  text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal">
             Bathroom Remodeling
           </ListItem>
         </div>
       </Typography>
+        </div>
+      
 
       <div className="mt-[6px]">
         <OurServiceManu />
       </div>
 
+      <div className="border-y border-black">
       <Typography
         as={Link}
         href="/contact-us"
@@ -225,11 +238,13 @@ function NavList() {
         className="font-medium rounded-full text-sm md:text-sm xl:text-base 2xl:text-lg"
       >
         <div>
-          <ListItem className="hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal ">
+          <ListItem className="px-0 hover:bg-transparent hover:text-white py-1 font-family-secondary flex items-start md:items-center text-lg md:text-base 5xl:text-lg text-black md:text-white font-bold leading-normal ">
             Contact Us
           </ListItem>
         </div>
       </Typography>
+        </div>
+      
     </List>
   );
 }
@@ -289,25 +304,25 @@ export function NavHeader() {
                         Follow Us on:
                       </p>
                       <div className="flex items-center gap-3">
-                      <div className="bg-primary-50 p-2 rounded-full">
-                        <FaYoutube className="w-[18px] h-[18px] text-white" />
-                      </div>
-                      <div className="bg-primary-50 p-2 rounded-full">
-                        <FaInstagram className="w-[18px] h-[18px] text-white" />
-                      </div>
-                      <div className="bg-primary-50 p-2 rounded-full">
-                        <FaLinkedin className="w-[18px] h-[18px] text-white" />
-                      </div>
-                      <div className="bg-primary-50 p-2 rounded-full">
-                        <FaFacebook className="w-[18px] h-[18px] text-white" />
-                      </div>
+                        <div className="bg-primary-50 p-2 rounded-full">
+                          <FaYoutube className="w-[18px] h-[18px] text-white" />
+                        </div>
+                        <div className="bg-primary-50 p-2 rounded-full">
+                          <FaInstagram className="w-[18px] h-[18px] text-white" />
+                        </div>
+                        <div className="bg-primary-50 p-2 rounded-full">
+                          <FaLinkedin className="w-[18px] h-[18px] text-white" />
+                        </div>
+                        <div className="bg-primary-50 p-2 rounded-full">
+                          <FaFacebook className="w-[18px] h-[18px] text-white" />
+                        </div>
                       </div>
                     </div>
 
                     <div>
                       <button className="flex items-center gap-[8px] 5xl:gap-[12px]">
                         <div className="bg-primary-50 p-2 rounded-lg">
-                          <LuPhoneCall className="w-[18px] h-[18px] text-white"/>
+                          <LuPhoneCall className="w-[18px] h-[18px] text-white" />
                         </div>
                         <Link
                           className="font-family-secondary text-lg font-bold text-white leading-normal"
@@ -334,19 +349,10 @@ export function NavHeader() {
             hideOnScroll ? "hidden" : ""
           }`}
         >
-          {/* <div className="bg-secondary py-[6px]">
-            <p className="font-family-secondary text-lg font-bold text-white leading-normal text-center">
-              FLOORS NOW, PAY OVER TIME
-            </p>
-            <p className="font-family-secondary text-sm font-bold text-white leading-normal text-center">
-              Convenient monthly payments to fit your budget1
-            </p>
-            
-          </div> */}
         </div>
 
         <div className="block md:hidden w-full bg__color py-[5px] md:py-0 flex justify-between items-center px-[20px] sm:px-[30px] -mt-1">
-          <div className=" w-1/2">
+          <div className=" w-1/4">
             <Image
               src="/assets/glitz/newHomePage/nav/nav__logo__glitz.png"
               alt="nav logo"
@@ -366,13 +372,7 @@ export function NavHeader() {
               {openNav ? (
                 <XMarkIcon className="h-10 w-10" strokeWidth={2} />
               ) : (
-                <Image
-                  src="/assets/glitz/newHomePage/icons/menu_icon.png"
-                  width={35}
-                  height={29}
-                  alt="Menu"
-                  className="w-[35px] h-[29px]"
-                />
+                <IoMenuSharp className="text-primary-50 w-[35px] h-[35px]"/>
               )}
             </IconButton>
           </div>
@@ -415,67 +415,29 @@ export function NavHeader() {
             <span className="sr-only">Menu</span>
           </IconButton>
         </div> */}
-        <NavList />
-        <div className="w-full flex flex-col justify-start items-center pt-[20px] pl-[17px]">
-          <div className="w-full flex justify-start">
-            <button className="flex justify-start items-center gap-[6px] md:gap-[3px] 5xl:gap-[8px]">
-              {" "}
-              <Image
-                src="/assets/glitz/icons/nav_phone.png"
-                alt="phone icon"
-                width={45}
-                height={45}
-                className="w-[45px] h-[45px] icon__bg p-1"
-              />
-              <Link
-                className="font-family-secondary text-lg 2xl:text-[20px] 5xl:text-[25px] font-bold text-black leading-normal"
-                href="tel:(281) 758-5450"
-              >
-                (281) 758-5450
-              </Link>
-            </button>
-          </div>
-          {/* second div  */}
-          <div className="flex items-center gap-[6px] md:gap-[3px] 5xl:gap-[8px] py-[30px]">
+        <div className="mb-4">
             <Image
-              src="/assets/glitz/icons/nav_map.png"
-              alt="phone icon"
-              width={45}
-              height={45}
-              className="w-[45px] h-[45px] icon__bg p-1"
+              src="/assets/glitz/newHomePage/nav/glitz_nav_logo.png"
+              alt="nav logo"
+              width={142}
+              height={91}
+              className="w-full"
             />
-            <div>
-              <p className="font-family-secondary text-sm font-normal text-black leading-normal">
-                Location
-              </p>
-              <p className="font-family-secondary text-sm 5xl:text-base font-bold text-black leading-normal pt-[3px] 5xl:pt-[5px]">
-                <Link href="location:16726 Huffmeister Road, D400, Cypress, Tx 77429">
-                  16726 Huffmeister Road, D400, Cypress, Tx 77429
-                </Link>
-              </p>
-            </div>
           </div>
-
-          <div className="w-full flex justify-start">
-            <button className="font-family-secondary flex items-center gap-[5px] text-sm 5xl:text-base font-bold text-primary leading-normal py-[8px] px-[14px] border-[1.5px] border-secondary rounded-[6px]">
-              <Link className="text-primary" href="tel:(281) 758-5450">
-                Get Free Quote
-              </Link>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="12"
-                viewBox="0 0 18 12"
-                fill="none"
-              >
-                <path
-                  d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75L1 5.25ZM17.5303 6.53033C17.8232 6.23744 17.8232 5.76256 17.5303 5.46967L12.7574 0.696699C12.4645 0.403806 11.9896 0.403806 11.6967 0.696699C11.4038 0.989593 11.4038 1.46447 11.6967 1.75736L15.9393 6L11.6967 10.2426C11.4038 10.5355 11.4038 11.0104 11.6967 11.3033C11.9896 11.5962 12.4645 11.5962 12.7574 11.3033L17.5303 6.53033ZM1 6.75L17 6.75V5.25L1 5.25L1 6.75Z"
-                  fill="#905C4C"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
+        <NavList />
+        <div className="mt-8">
+                      <button className="flex items-center gap-[8px] 5xl:gap-[12px]">
+                        <div className="bg-primary-50 p-2 rounded-lg">
+                          <LuPhoneCall className="w-[18px] h-[18px] text-white" />
+                        </div>
+                        <Link
+                          className="font-family-secondary text-lg font-bold text-black leading-normal"
+                          href="tel:(346) 445-6343"
+                        >
+                          (346) 445-6343
+                        </Link>
+                      </button>
+                    </div>
       </Drawer>
     </>
   );
--- src/components/__shared_one_time/HomePage/OnlyHomePage/RemodelingServices/RemodelingServices.jsx
+++ src/components/__shared_one_time/HomePage/OnlyHomePage/RemodelingServices/RemodelingServices.jsx
@@ -16,7 +16,7 @@ const RemodelingServices = () => {
     if (showBathroomOverlay) {
       bathroomTimer = setTimeout(() => {
         setShowBathroomOverlay(false);
-      }, 3000);
+      }, 5000);
     }
     return () => clearTimeout(bathroomTimer);
   }, [showBathroomOverlay]);
@@ -26,7 +26,7 @@ const RemodelingServices = () => {
     if (showKitchenOverlay) {
       kitchenTimer = setTimeout(() => {
         setShowKitchenOverlay(false);
-      }, 3000);
+      }, 5000);
     }
     return () => clearTimeout(kitchenTimer);
   }, [showKitchenOverlay]);
@@ -174,12 +174,12 @@ const RemodelingServices = () => {
 
           {/* Overlay Content */}
           {showBathroomOverlay && (
-            <div className=" absolute inset-0 bg-black/90 text-white p-4 flex flex-col justify-center">
-              <h2 className="text-xl font-bold mb-3">Bathroom Remodeling</h2>
-              <p>
-                Your bathroom should be more than just functional—it should be a
-                relaxing sanctuary that combines style and comfort.
+            <div className="h-2/3 absolute top-[33%] inset-0 bg-black/90 text-white p-4 flex flex-col justify-center">
+              <h2 className="text-lg 5xl:text-[20px] font-bold leading-[40px] text-white text-center">Bathroom Remodeling</h2>
+              <p className="text-base 5xl:text-lg font-normal text-center leading-normal text-white">
+              Your bathroom should be more than just functional—it should be a relaxing sanctuary that combines style and comfort. At Glitz Floors & More, we specialize in transforming outdated bathrooms into modern,
               </p>
+              <button className="mt-3 flex justify-center items-center gap-2 text-white text-sm font-bold leading-[20px] underline">Bathroom Remodeling <FaArrowRightLong className="mt-1"/></button>
             </div>
           )}
         </div>
@@ -215,12 +215,12 @@ const RemodelingServices = () => {
 
             {/* Overlay Content */}
             {showKitchenOverlay && (
-              <div className=" absolute inset-0 bg-black/90 text-white p-4 flex flex-col justify-center">
-                <h2 className="text-xl font-bold mb-3">Kitchen Remodeling</h2>
-                <p>
-                  Your bathroom should be more than just functional—it should be
-                  a relaxing sanctuary that combines style and comfort.
+              <div className="h-2/3 absolute top-[33%] inset-0 bg-black/90 text-white p-4 flex flex-col justify-center">
+                <h2 className="text-lg 5xl:text-[20px] font-bold leading-[40px] text-white text-center">Kitchen Remodeling</h2>
+                <p className="text-base 5xl:text-lg font-normal text-center leading-normal text-white">
+                The kitchen is the heart of your home—a place where meals are shared, memories are made, and creativity comes to life. At Glitz Floors & More, we specialize in crafting kitchens that are as functional as they are beautiful.
                 </p>
+                <button className="mt-3 flex justify-center items-center gap-2 text-white text-sm font-bold leading-[20px] underline">Kitchen Remodeling <FaArrowRightLong className="mt-1"/></button>
               </div>
             )}
           </div>
