fafaa081e0e075b5c4f0b3b1f1acce052136abb0
Oliulla <mohammad.oliullah25@gmail.com>
2024-11-28 22:35:05 +0600

    chagnes commit

--- src/components/__shared_one_time/HomePage/Homepage.jsx
+++ src/components/__shared_one_time/HomePage/Homepage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import HomeBanner from "./HomeBanner/HomeBanner";
+import Flooring from "./Flooring/Flooring";
+import Gallery from "./Gallery/Gallery";
+import YourDream from "./YourDream/YourDream";
 import ContactUs from "./ContactUs/ContactUs";
 import ServicesArea from "./ServicesArea/ServicesArea";
 
 const HomePage = () => {
   return (
     <div className="custom-container">
+      <section>
+        <HomeBanner />
+      </section>
+
+      <section>
+        <Flooring />
+      </section>
+
+      <section>
+        <Gallery />
+      </section>
+
+      <section>
+        <YourDream/>
+      </section>
 
       <section>
         <ContactUs/>
