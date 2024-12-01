21baac2e0326aee3964fcb83d6380f74d5b38073
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-02 00:17:08 +0600

    footer section design change

--- src/components/__layouts/Footers/Footer.jsx
+++ src/components/__layouts/Footers/Footer.jsx
@@ -4,11 +4,15 @@ import Link from "next/link";
 import Image from "next/image";
 import "./footer.css";
 
-const socialIons = [
-  "/assets/glitz/images/instragram.png",
-  "/assets/glitz/images/twitter.png",
-  "/assets/glitz/images/facebook.png"
-]
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { CiClock2 } from "react-icons/ci";
+import { IoSettingsSharp } from "react-icons/io5";
+
 
 const Footer = () => {
   return (
@@ -20,49 +24,44 @@ const Footer = () => {
             {/* first div  */}
             <div className="col-span-2">
               <Image
-                src="/assets/glitz/newHomePage/black_glitz_logo.png"
+                src="/assets/glitz/newHomePage/nav/nav__logo__glitz.png"
                 alt="footer logo"
                 width={144}
                 height={148}
                 className="w-full"
               />
-              <p className="mt-2 font-family-primary text-base 5xl:text-lg font-normal text-black text-left leading-[30px]">
+              <p className="mt-2 font-family-primary text-base 5xl:text-lg font-normal text-white text-left leading-[30px]">
                 If you're in the Cypress area and looking for expert flooring
                 and remodeling services, contact us today.
               </p>
             </div>
             {/* second div  */}
             <div className="col-span-2">
-              <p className="font-family-primary flex items-center justify-start gap-[10px] 5xl:gap-[16px] text-lg font-bold text-black pb-[15px] 5xl:pb-[17px] leading-[29px]">
-                <Image
-                  src="/assets/glitz/icons/clock.png"
-                  alt="clock icon"
-                  width={24}
-                  height={24}
-                />
+              <p className="font-family-primary flex items-center justify-start gap-[10px] 5xl:gap-[16px] text-lg font-bold text-white pb-[15px] 5xl:pb-[17px] leading-[29px]">
+                <CiClock2 className="w-[24px] h-[24px]"/>
                 Hours of Operations:
               </p>
-              <ul className="text-black font-family-secondary">
-                <li className="text-base 5xl:text-lg font-normal text-black leading-[27px]">
+              <ul className="text-white font-family-secondary">
+                <li className="text-base 5xl:text-lg font-normal text-white leading-[27px]">
                   Monday to Thursday
                 </li>
-                <li className="text-base 5xl:text-lg font-normal text-black leading-[27px] py-[10px] 5xl:py-[14px]">
+                <li className="text-base 5xl:text-lg font-normal text-white leading-[27px] py-[10px] 5xl:py-[14px]">
                   10:00 am - 06:00 p.m.
                 </li>
-                <li className="text-base 5xl:text-lg font-normal text-black leading-[27px]">
+                <li className="text-base 5xl:text-lg font-normal text-white leading-[27px]">
                   Friday{" "}
                 </li>
-                <li className="text-base 5xl:text-lg font-normal text-black leading-[27px] py-[10px] 5xl:py-[14px]">
+                <li className="text-base 5xl:text-lg font-normal text-white leading-[27px] py-[10px] 5xl:py-[14px]">
                   10:00 am - 05:00 p.m.
                 </li>
-                <li className="text-base 5xl:text-lg font-normal text-black leading-[27px]">
+                <li className="text-base 5xl:text-lg font-normal text-white leading-[27px]">
                   Saturday
                 </li>
-                <li className="text-base 5xl:text-lg font-normal text-black leading-[27px] py-[10px] 5xl:py-[14px]">
+                <li className="text-base 5xl:text-lg font-normal text-white leading-[27px] py-[10px] 5xl:py-[14px]">
                   10:00 am - 02:00 p.m.
                 </li>
               </ul>
-              <p className="font-family-secondary text-sm font-normal text-black leading-[21px]">
+              <p className="font-family-secondary text-sm font-normal text-white leading-[21px]">
                 * We remain open during statutoryholidays and operate as per our
                 regular hours.
               </p>
@@ -70,29 +69,24 @@ const Footer = () => {
             {/* third div  */}
             <div className="col-span-2">
               <div className="">
-                <p className="font-family-primary flex items-center justify-start gap-[10px] 5xl:gap-[16px] text-lg font-bold text-black pb-[17px] leading-[29px]">
-                  <Image
-                    src="/assets/glitz/icons/setting.png"
-                    alt="setting icon"
-                    width={24}
-                    height={24}
-                  />
+                <p className="font-family-primary flex items-center justify-start gap-[10px] 5xl:gap-[16px] text-lg font-bold text-white pb-[17px] leading-[29px]">
+                  <IoSettingsSharp className="w-[24px] h-[24px]"/>
                   Our Services
                 </p>
                 <ul className="font-family-secondary">
-                  <li className="text-base 5xl:text-lg font-normal text-black leading-[27px]">
+                  <li className="text-base 5xl:text-lg font-normal text-white leading-[27px]">
                     &gt; Flooring Installations
                   </li>
-                  <li className="text-base 5xl:text-lg font-normal text-black leading-[27px] py-[10px] 5xl:py-[14px]">
+                  <li className="text-base 5xl:text-lg font-normal text-white leading-[27px] py-[10px] 5xl:py-[14px]">
                     &gt; Kitchen Remodeling
                   </li>
-                  <li className="text-base 5xl:text-lg font-normal text-black leading-[27px]">
+                  <li className="text-base 5xl:text-lg font-normal text-white leading-[27px]">
                     &gt; Bathroom Remodeling
                   </li>
-                  <li className="text-base 5xl:text-lg font-normal text-black leading-[27px] py-[10px] 5xl:py-[14px]">
+                  <li className="text-base 5xl:text-lg font-normal text-white leading-[27px] py-[10px] 5xl:py-[14px]">
                     &gt; Interior Painting
                   </li>
-                  <li className="text-base 5xl:text-lg font-normal text-black leading-[27px]">
+                  <li className="text-base 5xl:text-lg font-normal text-white leading-[27px]">
                     &gt; Interior Remodel
                   </li>
                 </ul>
@@ -103,14 +97,9 @@ const Footer = () => {
             <div className="col-span-2">
               <ul className="font-family-secondary">
                 <li className="flex items-start gap-[12px] 5xl:gap-[24px] text-base 5xl:text-lg font-normal leading-[29px]">
-                  <Image
-                    src="/assets/glitz/icons/m.png"
-                    alt="map icon"
-                    width={24}
-                    height={24}
-                  />
+                  <FaLocationDot className="w-[24px] h-[24px] mt-2"/>
                   <Link
-                    className="text-black"
+                    className="text-white"
                     href="location:16726 Huffmeister Rd D400, Cypress, TX 77429, United States"
                   >
                     16726 Huffmeister Rd <br className="hidden xl:block" />{" "}
@@ -119,25 +108,15 @@ const Footer = () => {
                   </Link>
                 </li>
                 <li className="flex items-center gap-[12px] 5xl:gap-[24px] text-base 5xl:text-lg font-normal leading-[29px] py-[25px] 5xl:py-[27px]">
-                  <Image
-                    src="/assets/glitz/icons/p.png"
-                    alt="phone icon"
-                    width={24}
-                    height={24}
-                  />
-                  <Link className="text-black" href="tel:(281) 758-5450">
+                  <FaPhoneVolume className="w-[24px] h-[18px]"/>
+                  <Link className="text-white" href="tel:(281) 758-5450">
                   (281) 758-5450
                   </Link>
                 </li>
                 <li className="flex items-center gap-[12px] 5xl:gap-[24px] text-base 5xl:text-lg font-normal leading-[29px]">
-                  <Image
-                    src="/assets/glitz/icons/email.png"
-                    alt="email icon"
-                    width={24}
-                    height={18}
-                  />
+                  <MdOutlineEmail className="w-[24px] h-[24px]"/>
                   <Link
-                    className="text-black"
+                    className="text-white"
                     href="mail:info@glitzfloors&more.us"
                   >
                     info@glitzfloors&more.us
@@ -145,11 +124,9 @@ const Footer = () => {
                 </li>
               </ul>
               <div className="flex items-center gap-[30px] pt-[29px]">
-                {
-                  socialIons?.map((img, index)=>{
-                    return <Image key={index} src={img} alt="social icon" width={21} height={21}/>
-                  })
-                }
+                <FaInstagram className="w-[21px] h-[21px]"/>
+                <FaXTwitter className="w-[21px] h-[21px]"/>
+                <FaFacebook className="w-[21px] h-[21px]"/>
               </div>
             </div>
           </div>
@@ -188,49 +165,44 @@ const Footer = () => {
           <div className="pt-[35px] px-[30px]">
             <Image
               // src="/assets/glitz/logo/nav_logo.png"
-              src="/assets/glitz/newHomePage/black_glitz_logo.png"
+              src="/assets/glitz/newHomePage/nav/nav__logo__glitz.png"
               alt="footer logo"
               width={144}
               height={148}
               className="w-1/2 mx-auto"
             />
-            <p className="font-family-primary text-lg font-normal text-black leading-[30px] text-center pt-2">
+            <p className="font-family-primary text-lg font-normal text-white leading-[30px] text-center pt-2">
               If you're in the Cypress area and looking for expert flooring and
               remodeling services, contact us today.
             </p>
           </div>
           {/* second div  */}
           <div className="pt-[20px] px-[30px]">
-            <p className="font-family-primary flex items-center justify-center gap-[10px] 5xl:gap-[16px] text-lg text-center font-bold text-black pb-[17px] leading-[29px]">
-              <Image
-                src="/assets/glitz/icons/clock.png"
-                alt="clock icon"
-                width={24}
-                height={24}
-              />
+            <p className="font-family-primary flex items-center justify-center gap-[10px] 5xl:gap-[16px] text-lg text-center font-bold text-white pb-[17px] leading-[29px]">
+            <CiClock2 className="w-[24px] h-[24px]"/>
               Hours of Operations:
             </p>
             <ul className="text-white font-family-secondary">
-              <li className="text-base md:text-lg text-center normal text-black leading-[27px]">
+              <li className="text-base md:text-lg text-center normal text-white leading-[27px]">
                 Monday to Thursday
               </li>
-              <li className="text-base md:text-lg text-center font-normal text-black leading-[27px] py-[10px]">
+              <li className="text-base md:text-lg text-center font-normal text-white leading-[27px] py-[10px]">
                 10:00 am - 06:00 p.m.
               </li>
-              <li className="text-base md:text-lg text-center font-normal text-black leading-[27px]">
+              <li className="text-base md:text-lg text-center font-normal text-white leading-[27px]">
                 Friday{" "}
               </li>
-              <li className="text-base md:text-lg text-center font-normal text-black leading-[27px] py-[10px]">
+              <li className="text-base md:text-lg text-center font-normal text-white leading-[27px] py-[10px]">
                 10:00 am - 05:00 p.m.
               </li>
-              <li className="text-base md:text-lg text-center font-normal text-black leading-[27px]">
+              <li className="text-base md:text-lg text-center font-normal text-white leading-[27px]">
                 Saturday
               </li>
-              <li className="text-base md:text-lg text-center font-normal text-black leading-[27px] py-[10px]">
+              <li className="text-base md:text-lg text-center font-normal text-white leading-[27px] py-[10px]">
                 10:00 am - 02:00 p.m.
               </li>
             </ul>
-            <p className="font-family-secondary text-sm font-normal text-center text-black leading-[21px]">
+            <p className="font-family-secondary text-sm font-normal text-center text-white leading-[21px]">
               * We remain open during statutoryholidays and operate as per our
               regular hours.
             </p>
@@ -238,29 +210,24 @@ const Footer = () => {
           {/* third div  */}
           <div className="pt-[20px] px-[30px]">
             <div className="">
-              <p className="font-family-primary flex items-center justify-center gap-[10px] 5xl:gap-[16px] text-lg text-center font-bold text-black pb-[17px] leading-[29px]">
-                <Image
-                  src="/assets/glitz/icons/setting.png"
-                  alt="setting icon"
-                  width={24}
-                  height={24}
-                />
+              <p className="font-family-primary flex items-center justify-center gap-[10px] 5xl:gap-[16px] text-lg text-center font-bold text-white pb-[17px] leading-[29px]">
+              <IoSettingsSharp className="w-[24px] h-[24px]"/>
                 Our Services
               </p>
               <ul className="font-family-secondary">
-                <li className="text-base md:text-lg text-center font-normal text-black leading-[27px]">
+                <li className="text-base md:text-lg text-center font-normal text-white leading-[27px]">
                   &gt; Flooring Installations
                 </li>
-                <li className="text-base md:text-lg text-center font-normal text-black leading-[27px] py-[10px]">
+                <li className="text-base md:text-lg text-center font-normal text-white leading-[27px] py-[10px]">
                   &gt; Kitchen Remodeling
                 </li>
-                <li className="text-base md:text-lg text-center font-normal text-black leading-[27px]">
+                <li className="text-base md:text-lg text-center font-normal text-white leading-[27px]">
                   &gt; Bathroom Remodeling
                 </li>
-                <li className="text-base md:text-lg text-center font-normal text-black leading-[27px] py-[10px]">
+                <li className="text-base md:text-lg text-center font-normal text-white leading-[27px] py-[10px]">
                   &gt; Interior Painting
                 </li>
-                <li className="text-base md:text-lg text-center font-normal text-black leading-[27px]">
+                <li className="text-base md:text-lg text-center font-normal text-white leading-[27px]">
                   &gt; Interior Remodel
                 </li>
               </ul>
@@ -270,14 +237,9 @@ const Footer = () => {
           <div className="pt-[20px] px-[30px]">
             <ul className="font-family-secondary">
               <li className="flex justify-center items-center gap-[24px] text-base 5xl:text-lg font-normal leading-[29px]">
-                <Image
-                  src="/assets/glitz/icons/m.png"
-                  alt="map icon"
-                  width={21}
-                  height={21}
-                />
+              <FaLocationDot className="w-[24px] h-[24px] mt-2"/>
                 <Link
-                  className="text-black"
+                  className="text-white"
                   href="location:16726 Huffmeister Rd D400, Cypress, TX 77429, United States"
                 >
                   16726 Huffmeister Rd <br className="hidden 5xl:block" /> D400,
@@ -286,25 +248,15 @@ const Footer = () => {
                 </Link>
               </li>
               <li className="flex justify-center items-center gap-[24px] text-base 5xl:text-lg font-normal leading-[29px] py-[20px]">
-                <Image
-                  src="/assets/glitz/icons/p.png"
-                  alt="phone icon"
-                  width={21}
-                  height={21}
-                />
-                <Link className="text-black" href="tel:(281) 758-5450">
+              <FaPhoneVolume className="w-[24px] h-[18px]"/>
+                <Link className="text-white" href="tel:(281) 758-5450">
                 (281) 758-5450
                 </Link>
               </li>
               <li className="flex justify-center items-center gap-[24px] text-base 5xl:text-lg font-normal leading-[29px]">
-                <Image
-                  src="/assets/glitz/icons/email.png"
-                  alt="email icon"
-                  width={21}
-                  height={21}
-                />
+              <MdOutlineEmail className="w-[24px] h-[24px]"/>
                 <Link
-                  className="text-black"
+                  className="text-white"
                   href="mail:info@glitzfloors&more.us"
                 >
                   info@glitzfloors&more.us
@@ -312,25 +264,23 @@ const Footer = () => {
               </li>
             </ul>
             <div className="flex justify-center items-center gap-[30px] pt-[20px]">
-            {
-                  socialIons?.map((img, index)=>{
-                    return <Image key={index} src={img} alt="social icon" width={21} height={21}/>
-                  })
-                }
+            <FaInstagram className="w-[21px] h-[21px]"/>
+                <FaXTwitter className="w-[21px] h-[21px]"/>
+                <FaFacebook className="w-[21px] h-[21px]"/>
               
             </div>
           </div>
           {/* fifth div  */}
           <div className="pt-[40px]">
             <div className="border border-black  mx-[30px]" />
-            <p className="font-family-primary text-base font-medium pb-[15px] text-black text-center pt-[20px]">
+            <p className="font-family-primary text-base font-medium pb-[15px] text-white text-center pt-[20px]">
               © All Copyright 2024 by GLITZ FLOORS & MORE
             </p>
             <div className="flex justify-evenly">
-              <p className="font-family-primary text-base font-medium text-black">
+              <p className="font-family-primary text-base font-medium text-white">
                 Terms & Condition
               </p>
-              <p className="font-family-primary text-base font-medium text-black">Privacy Policy</p>
+              <p className="font-family-primary text-base font-medium text-white">Privacy Policy</p>
             </div>
           </div>
           {/* footer bottom  */}
