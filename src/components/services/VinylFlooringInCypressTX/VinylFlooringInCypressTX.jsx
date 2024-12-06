4fa5f1f9fa767b0a1e2c9325dc15187f28e0cfd1
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-06 23:07:12 +0600

    brand, location , review and services area section add

--- src/components/services/VinylFlooringInCypressTX/VinylFlooringInCypressTX.jsx
+++ src/components/services/VinylFlooringInCypressTX/VinylFlooringInCypressTX.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React from "react";
 import "./VinylFlooringInCypressTX.css";
-import VinylFlooringInCypressTXBanner from './VinylFlooringInCypressTXBanner/VinylFlooringInCypressTXBanner';
+import VinylFlooringInCypressTXBanner from "./VinylFlooringInCypressTXBanner/VinylFlooringInCypressTXBanner";
+import Brand from "@/components/__shared_one_time/FlooringInstallation/Brand/Brand";
+import Review from "@/components/__shared_one_time/HomePage/OnlyHomePage/Review/Review";
+import ServicesArea from "@/components/__shared_one_time/HomePage/OnlyHomePage/ServicesArea/ServicesArea";
+import Location from "@/components/__shared_one_time/HomePage/OnlyHomePage/Location/Location";
 
 const VinylFlooringInCypressTX = () => {
-    return (
-        <div>
-            <section>
-                <VinylFlooringInCypressTXBanner/>
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      <section>
+        <VinylFlooringInCypressTXBanner />
+      </section>
+
+      <section>
+        <Brand />
+      </section>
+
+      <section>
+        <Review />
+      </section>
+
+      <section>
+        <ServicesArea />
+      </section>
+
+      {/* <section>
+        <ContactUs />
+      </section> */}
+
+      <section>
+        <Location />
+      </section>
+
+    </div>
+  );
 };
 
 export default VinylFlooringInCypressTX;
