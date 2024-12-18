cf4600e7fd8d5c65d37c3044b9838671dadfc8c7
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-18 15:30:09 +0600

    about us and banner section add

--- src/components/services/PaintingContractorsAllComponent/PaintingContractorsAllComponent.jsx
+++ src/components/services/PaintingContractorsAllComponent/PaintingContractorsAllComponent.jsx
@@ -4,12 +4,22 @@ import Location from "@/components/__shared_one_time/HomePage/OnlyHomePage/Locat
 import Review from "@/components/__shared_one_time/HomePage/OnlyHomePage/Review/Review";
 import ServicesArea from "@/components/__shared_one_time/HomePage/OnlyHomePage/ServicesArea/ServicesArea";
 import React from "react";
+import PaintingContractorBanner from "./PaintingContractorBanner/PaintingContractorBanner";
+import AboutUs from "./AboutUs/AboutUs";
 
 const PaintingContractorsAllComponent = () => {
   return (
     <div>
       <section>
+        <PaintingContractorBanner/>
+      </section>
+      
+      <section>
         <Brand />
+      </section>
+
+      <section>
+        <AboutUs/>
       </section>
 
       <section>
