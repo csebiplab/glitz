a99ba8de3d42e7a4f359b631eaa68f353a6d4099
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-22 21:29:20 +0600

    loading add this project

---
+++ src/app/(pages)/loading.js
@@ -0,0 +1,13 @@
+import LoaderComponent from '@/components/common/LoaderComponent';
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <LoaderComponent/>
+            {/* loading .... */}
+        </div>
+    );
+};
+
+export default loading;
